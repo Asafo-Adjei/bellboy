6bb1ef25c9f27a2c1f562446cc158c2a40ddcbc9
Asafo-Adjei <yawadjei2003gmail.com>
2023-10-20 17:09:41 -0700

    added layout

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const routes = require('./routes');
 
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
@@ -11,5 +12,5 @@ app.use(routes);
 
 
 app.listen(3000, () => {
-    console.log('Listening on PORT : '+ 3000);
+    console.log('Listening on PORT : ' + 3000);
 })
