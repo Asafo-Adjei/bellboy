c5193548f7cc42ac312063d05fac96c38256a2dc
Sammah <sammahaddison@gmail.com>
2023-11-08 15:07:17 +0000

    WIP - added template page

--- routes.js
+++ routes.js
@@ -8,5 +8,13 @@ router.get("/add", function (req, res) {
   res.render("add-app");
 });
 
+router.get("/eat", function (req, res) {
+  res.render("hello");
+});
+
+router.get("/sam", function (req, res) {
+  res.render("topnav");
+});
+
 
 module.exports = router;
